69cb5c7336b154278c663e8720fab0c6eb6ba6f1
michael <michaeleburrell@gmail.com>
2019-10-16 14:46:32 -0700

    adds auth to movie frontend app

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,92 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="Movies">
+    <h1>New Movie</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieTitle" />
+      Year:
+      <input type="text" v-model="newMovieYear" />
+      Plot:
+      <input type="text" v-model="newMoviePlot" />
+      <button v-on:click="createMovie()">Create Movie</button>
+    </div>
+    <h1>All Movies</h1>
+    <div v-for="movie in movies">
+      <h2>{{ movie.title }}</h2>
+      <button v-on:click="showMovie(movie)">Show more</button>
+      <div v-if="currentMovie === movie">
+        <p>Year: {{ movie.year }}</p>
+        <p>Plot {{ movie.plot }}</p>
+        <div>
+          Title:
+          <input type="text" v-model="movie.title" />
+          Year:
+          <input type="text" v-model="movie.year" />
+          Plot:
+          <input type="text" v-model="movie.plot" />
+          <button v-on:click="updateMovie(movie)">Update Movie</button>
+          <button v-on:click="destroyMovie(movie)">Delete Movie</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      movies: [],
+      currentMovie: {},
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/movies").then(response => {
+      this.movies = response.data;
+    });
+  },
+  methods: {
+    createMovie: function() {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot
+      };
+      axios.post("/api/movies", params).then(response => {
+        this.movies.push(response.data);
+        this.newMovieTitle = "";
+        this.newMovieYear = "";
+        this.newMoviePlot = "";
+      });
+    },
+    showMovie: function(movie) {
+      if (this.currentMovie === movie) {
+        this.currentMovie = {};
+      } else {
+        this.currentMovie = movie;
+      }
+    },
+    updateMovie: function(movie) {
+      var params = {
+        title: movie.title,
+        year: movie.year,
+        plot: movie.plot
+      };
+      axios.patch("/api/movies/" + movie.id, params).then(response => {
+        this.currentMovie = {};
+      });
+    },
+    destroyMovie: function(movie) {
+      axios.delete("/api/movies/" + movie.id).then(response => {
+        var index = this.movies.indexOf(movie);
+        this.movies.splice(index, 1);
+      });
+    }
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>New Actor</h1>
+    <div>
+      First Name:
+      <input type="text" v-model="newActorFirstName" />
+      Last Name:
+      <input type="text" v-model="newActorLastName" />
+      Age:
+      <input type="text" v-model="newActorAge" />
+      known For:
+      <input type="text" v-model="newActorKnownFor" />
+
+      <button v-on:click="createActor()">Create Actor</button>
+    </div>
+    <h1>All Actors</h1>
+    <div v-for="actor in actors">
+      <h2>{{ actor.first_name }}</h2>
+      <h2>{{ actor.last_name }}</h2>
+      <button v-on:click="showActor(actor)">Show more</button>
+      <div v-if="currentActor === actor">
+        <p>Known For: {{ actor.known_for }}</p>
+        <div>
+          First Name:
+          <input type="text" v-model="actor.first_name" />
+          Last Name:
+          <input type="text" v-model="actor.last_name" />
+          Age:
+          <input type="text" v-model="actor.age" />
+          <button v-on:click="updateActor(actor)">Update Actor</button>
+          <button v-on:click="destroyActor(actor)">Delete Actor</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      actors: [],
+      currentActor: {},
+      newActorFirstName: "",
+      newActorLastName: "",
+      newActorAge: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/actors").then(response => {
+      this.actors = response.data;
+    });
+  },
+  methods: {
+    createActor: function() {
+      var params = {
+        first_name: this.newActorFirstName,
+        last_name: this.newActorLastName,
+        age: this.newActorAge
+      };
+      axios.post("/api/actors", params).then(response => {
+        this.actors.push(response.data);
+        this.newActorFirstName = "";
+        this.newActorLastName = "";
+        this.newActorAge = "";
+      });
+    },
+    showActor: function(actor) {
+      if (this.currentActor === actor) {
+        this.currentActor = {};
+      } else {
+        this.currentActor = actor;
+      }
+    },
+    updateActor: function(actor) {
+      var params = {
+        first_name: actor.first_name,
+        last_name: actor.last_name,
+        age: actor.age
+      };
+      axios.patch("/api/actors/" + actor.id, params).then(response => {
+        this.currentActor = {};
+      });
+    },
+    destroyActor: function(actor) {
+      axios.delete("/api/actors/" + actor.id).then(response => {
+        var index = this.actors.indexOf(actor);
+        this.actors.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
